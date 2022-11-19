c210643ad107e0b389515caad1e0409bd8f65b31
Naim Shaqqou <naeemshaqqou@gmail.com>
2022-11-19 18:03:29 -0500

    removed unneeded api headers

--- server.js
+++ server.js
@@ -21,21 +21,6 @@ api.setApp(app);
 //   .then(() => console.log("Mongo DB connected"))
 //   .catch(err => console.log(err));
 
-
-// app.use((req, res, next) => 
-// {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//   );
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'GET, POST, PATCH, DELETE, OPTIONS'
-//   );
-//   next();
-// });
-
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });

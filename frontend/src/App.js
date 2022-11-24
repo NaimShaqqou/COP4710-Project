0e58f209295b9c26d48a837e61dcdffaa3656da6
Naim Shaqqou <naeemshaqqou@gmail.com>
2022-11-24 16:50:31 -0500

    implement material ui theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './App.css';
-import LoginPage from './pages/LoginPage';
+import LoginPage from './pages/Authentication/LoginPage';
+
+// import {
+//   createTheme,
+//   ThemeProvider,
+// } from "@mui/material";
+
 function App() 
 {
+
+  // const theme = createTheme({
+  //   typography: {
+  //     fontFamily: [
+  //       'Comfortaa',
+  //       'Roboto',
+  //       '"Helvetica"',
+  //       'Arial',
+  //       'sans-serif'
+  //     ].join(','),
+  //   },
+  // });
+
   return (
-    <LoginPage />
+    // <ThemeProvider theme={theme}>
+      <LoginPage />
+    // </ThemeProvider>
   );
 }
 export default App;

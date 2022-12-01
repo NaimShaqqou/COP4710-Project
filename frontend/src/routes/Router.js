37069bd74b2300297a90f1ae3ee8a76e5b426151
Jared Wilson <jaredmwilsonn@knights.ucf.edu>
2022-12-01 16:09:29 -0500

    Add routing to CreateSurvey

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 // Pages import
 import LoginPage from "../pages/Authentication/LoginPage";
 import RegisterPage from "../pages/Authentication/RegisterPage";
+import CreateSurveyPage from "../pages/CreateSurveyPage";
 import Home from "../pages/Home";
 
 function Router() {
@@ -12,6 +13,7 @@ function Router() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/createSurvey" element={<CreateSurveyPage/>} />
     </Routes>
   );
 }
